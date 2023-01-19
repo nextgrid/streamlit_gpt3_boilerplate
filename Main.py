6b64f0aca0c8cef07d8964b28d8d56f90bb0e69a
benjaminchong99 <pschong261199@hotmail.com>
2023-01-19 18:19:55 +0800

    added region options

--- Main.py
+++ Main.py
@@ -19,42 +19,50 @@ def app():
         # Setting up the Title
         st.title("Write a poem based on these words")
 
-        poets_list = [" ", "shakespeare", "confucious",
-                      "seamus heaney", "edgar allan poe"]
+        # available options for poets and nouns
+        region_list = ["Asia", "Europe"]
+
         nouns_list = ["sutd", "university education",
                       "engineering", "STEM", "test", "examinations"]
 
-        # st.write("---")
-        # st.text_area(
-        #     "Use the example below or input your own text in English",
-        #     key="textbox",
-        #     max_chars=150
-        # )
-
         with st.sidebar:
-            poet_option = st.selectbox(
-                "Available Poets", poets_list)
+            region_option = st.selectbox(
+                "Select a region", region_list)
+
+            #  selected region will release the available choices
+            if region_option == "Asia":
+                poet_option = st.radio("Available Genre", options=[
+                    "Haiku", "Syair", "Yadu", "Sijo", "Kural"])  # Japan, Malaysia, Myanmar, Korea, India & Sri Lanka
+            elif region_option == "Europe":
+                poet_option = st.radio(
+                    "Available Genre", options=["Victorian", "Edwardian", "Georgian", "Surrealistic", "Shakespearean"])
+
+            # choices available for nouns
             noun_option = st.multiselect("Available Nouns", nouns_list)
-            poet_option
             noun_option
 
-            if st.button("Confirm choices"):
-                # cur_value = st.session_state.textbox
-                selected = str(noun_option)
-                filtered = selected.replace("'", "")
-                filtered = filtered.replace("[", "")
-                filtered = filtered.replace("]", "")
+            if st.button("Confirm choices"):  # finalised decision
+                # string the list
+                filtered = ""
+                for noun in noun_option:
+                    filtered += f"{noun}, "
+                filtered = filtered.strip()
+                if len(filtered) > 0:
+                    if filtered[-1] == ",":
+                        filtered = filtered[:-1]
+                    # st.write(filtered) # debug
 
                 st.session_state.textbox = "Write a " + poet_option + \
                     " poem about the benefits of " + filtered
 
-        # st.write("---")
+        # for initializing textbox
         st.text_area(
             "Use the example below or input your own text in English",
             key="textbox",
             max_chars=150
         )
 
+        # submit button
         if st.button("Submit"):
             with st.spinner(text="In progress"):
                 report_text = process_prompt(st.session_state.textbox)
