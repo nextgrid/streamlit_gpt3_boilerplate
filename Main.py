259d683a3bf72b8344b0294610769fe5cabe625d
benjaminchong99 <pschong261199@hotmail.com>
2023-01-19 17:26:03 +0800

    Edited selection choices
    changed format of sidebar choices into dropdown version. Working
    version with limited choices

--- Main.py
+++ Main.py
@@ -1,14 +1,6 @@
 import streamlit as st
 from model import GeneralModel
 
-def press_poet_button(button_label):
-    if st.button(button_label):
-        cur_value = st.session_state.textbox 
-        st.session_state.textbox = cur_value+ "Write a " + button_label + " poem about the benefits of "
-def press_noun_button(button_label):
-    if st.button(button_label):
-        cur_value = st.session_state.textbox 
-        st.session_state.textbox = cur_value + button_label
 
 def app():
 
@@ -17,37 +9,55 @@ def app():
 
     api_key = st.sidebar.text_input("APIkey", type="password")
     # Using the streamlit cache
+
     @st.cache
     def process_prompt(inp):
-        return pred.model_prediction(input=str(inp).strip() , api_key=api_key)
+        return pred.model_prediction(input=str(inp).strip(), api_key=api_key)
 
     if api_key:
 
         # Setting up the Title
         st.title("Write a poem based on these words")
 
-        poets_list = ["shakespeare", "confucious", "seamus heaney", "edgar allan poe"]
-        nouns_list = ["sutd", "university education", "engineering", "STEM", "test"]
+        poets_list = [" ", "shakespeare", "confucious",
+                      "seamus heaney", "edgar allan poe"]
+        nouns_list = ["sutd", "university education",
+                      "engineering", "STEM", "test", "examinations"]
+
+        # st.write("---")
+        # st.text_area(
+        #     "Use the example below or input your own text in English",
+        #     key="textbox",
+        #     max_chars=150
+        # )
 
         with st.sidebar:
-            st.write("all poets")
-            for poet in poets_list:
-                press_poet_button(poet)
-            
-            st.write("all nouns/descriptions")
-            for noun in nouns_list:
-                press_noun_button(noun)
+            poet_option = st.selectbox(
+                "Available Poets", poets_list)
+            noun_option = st.multiselect("Available Nouns", nouns_list)
+            poet_option
+            noun_option
+
+            if st.button("Confirm choices"):
+                # cur_value = st.session_state.textbox
+                selected = str(noun_option)
+                filtered = selected.replace("'", "")
+                filtered = filtered.replace("[", "")
+                filtered = filtered.replace("]", "")
+
+                st.session_state.textbox = "Write a " + poet_option + \
+                    " poem about the benefits of " + filtered
 
         # st.write("---")
         st.text_area(
-            "Use the example below or input your own text in English", 
+            "Use the example below or input your own text in English",
             key="textbox",
             max_chars=150
         )
 
         if st.button("Submit"):
             with st.spinner(text="In progress"):
-                report_text = process_prompt(st.session_state.textbox )
+                report_text = process_prompt(st.session_state.textbox)
                 st.markdown(report_text)
     else:
         st.error("🔑 Please enter API Key")
