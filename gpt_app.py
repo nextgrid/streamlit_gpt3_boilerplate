30ff8550cacc26f01511621d49e67063087e1f5b
benjaminchong99 <pschong261199@hotmail.com>
2023-01-24 18:21:36 +0800

    minor bug fixed

--- gpt_app.py
+++ gpt_app.py
@@ -2,12 +2,13 @@ import Main
 import streamlit as st
 from model import GeneralModel
 
-st.set_page_config(page_title="GPT-3 Boilerplate", page_icon=":shark:", layout="wide")
+# st.set_page_config(page_title="GPT-3 Boilerplate", page_icon=":shark:", layout="wide")
 if "api_key" not in st.session_state:
-  st.session_state['api_key'] = ""
+    st.session_state['api_key'] = ""
 if "pred" not in st.session_state:
-  st.session_state['pred'] = GeneralModel()
-st.set_page_config(page_title="Write a Poem: OH23", page_icon=":shark:", layout="wide")
+    st.session_state['pred'] = GeneralModel()
+st.set_page_config(page_title="Write a Poem: OH23",
+                   page_icon=":shark:", layout="wide")
 
 Main.app()
 
